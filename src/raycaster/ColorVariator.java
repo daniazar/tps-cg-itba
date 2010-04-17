a91c311b91e7c654b1d653121a14c18895a3e8fc
matiaspan <matiaspan@af5a1436-9db6-de8c-89f5-2511925b851f>
2010-04-17 14:42:57 +0000

    Delete

--- src/raycaster/ColorVariator.java
+++
@@ -1,9 +0,0 @@
-package raycaster;
-
-import java.awt.Color;
-
-public abstract class ColorVariator {
-	
-	public abstract Color changeColor(float baseDistance, Color baseColor,float distance);
-	
-}
--- src/raycaster/LightColorChooser.java
+++
@@ -1,9 +0,0 @@
-package raycaster;
-
-import java.awt.Color;
-
-public interface LightColorChooser {
-	
-	public Color getColor(Ray ray, float coef,  Color c);
-
-}
